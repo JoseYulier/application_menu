cb32dbed1c48a0675a63b08160045f72376cd13e
Jose Rodriguez Milanes <joseyulier@gmail.com>
2024-12-17 22:31:39 -0500

    MenuCascadaMejorado

--- lib/main.dart
+++ lib/main.dart
@@ -1,79 +1,225 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:io';
 
-void main() => runApp(const MyApp());
+void main() => runApp(const MenuAcceleratorApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyMenuBar extends StatelessWidget {
+  const MyMenuBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Menu cascada con submenus'),
+    return Column(
+      children: <Widget>[
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Expanded(
+              child: MenuBar(
+                children: <Widget>[
+                  SubmenuButton(
+                    menuChildren: <Widget>[
+                      MenuItemButton(
+                        leadingIcon: const Icon(Icons.save),
+                        trailingIcon: const Text('Ctrl + S',
+                            style: TextStyle(color: Colors.grey)),
+                        onPressed: () {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text('Save!'),
+                            ),
+                          );
+                        },
+                        child: const MenuAcceleratorLabel('&Save'),
+                      ),
+                      MenuItemButton(
+                        leadingIcon: const Icon(Icons.save),
+                        onPressed: () {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text('Save as!'),
+                            ),
+                          );
+                        },
+                        child: const MenuAcceleratorLabel('&Save as'),
+                      ),
+                      MenuItemButton(
+                        leadingIcon: const Icon(Icons.insert_drive_file),
+                        trailingIcon: const Text('Ctrl + O',
+                            style: TextStyle(color: Colors.grey)),
+                        onPressed: () {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text('Open file!'),
+                            ),
+                          );
+                        },
+                        child: const MenuAcceleratorLabel('&Open file'),
+                      ),
+                      MenuItemButton(
+                        leadingIcon: const Icon(Icons.folder),
+                        trailingIcon: const Text('Ctrl + F',
+                            style: TextStyle(color: Colors.grey)),
+                        onPressed: () {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text('Open folder!'),
+                            ),
+                          );
+                        },
+                        child: const MenuAcceleratorLabel('&Open folder'),
+                      ),
+                      SubmenuButton(
+                        menuChildren: <Widget>[
+                          MenuItemButton(
+                            leadingIcon: const Icon(Icons.shortcut),
+                            onPressed: () {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                  content: Text('Shortcuts!'),
+                                ),
+                              );
+                            },
+                            child: const MenuAcceleratorLabel('&Shortcuts'),
+                          ),
+                          MenuItemButton(
+                            leadingIcon: const Icon(Icons.extension),
+                            onPressed: () {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                  content: Text('Extensions!'),
+                                ),
+                              );
+                            },
+                            child: const MenuAcceleratorLabel('&Extensions'),
+                          ),
+                          SubmenuButton(
+                            leadingIcon: const Icon(Icons.format_paint),
+                            menuChildren: <Widget>[
+                              MenuItemButton(
+                                leadingIcon: const Icon(Icons.wb_sunny),
+                                onPressed: () {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                      content: Text('Light theme!'),
+                                    ),
+                                  );
+                                },
+                                child:
+                                    const MenuAcceleratorLabel('&Light theme'),
+                              ),
+                              MenuItemButton(
+                                leadingIcon: const Icon(Icons.wb_cloudy),
+                                onPressed: () {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                      content: Text('Dark theme!'),
+                                    ),
+                                  );
+                                },
+                                child:
+                                    const MenuAcceleratorLabel('&Dark theme'),
+                              ),
+                            ],
+                            child: const MenuAcceleratorLabel('&Change theme'),
+                          ),
+                        ],
+                        leadingIcon: const Icon(Icons.settings),
+                        child: const MenuAcceleratorLabel('&Preference'),
+                      ),
+                      MenuItemButton(
+                        onPressed: () {
+                          exit(0);
+                        },
+                        leadingIcon: const Icon(Icons.exit_to_app_rounded),
+                        child: const MenuAcceleratorLabel('&Quit'),
+                      ),
+                    ],
+                    child: const MenuAcceleratorLabel('&File'),
+                  ),
+                  SubmenuButton(
+                    menuChildren: <Widget>[
+                      MenuItemButton(
+                        onPressed: () {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text('Magnify!'),
+                            ),
+                          );
+                        },
+                        child: const MenuAcceleratorLabel('&Magnify'),
+                      ),
+                      MenuItemButton(
+                        onPressed: () {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text('Minify!'),
+                            ),
+                          );
+                        },
+                        child: const MenuAcceleratorLabel('Mi&nify'),
+                      ),
+                    ],
+                    child: const MenuAcceleratorLabel('&Edit'),
+                  ),
+                  SubmenuButton(
+                    menuChildren: <Widget>[
+                      MenuItemButton(
+                        onPressed: () {
+                          showAboutDialog(
+                            context: context,
+                            applicationName: 'MenuBar Sample',
+                            applicationVersion: '1.0.0',
+                          );
+                        },
+                        child: const MenuAcceleratorLabel('&About Version'),
+                      ),
+                      MenuItemButton(
+                        onPressed: () {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text('Other!'),
+                            ),
+                          );
+                        },
+                        child: const MenuAcceleratorLabel('&Other'),
+                      ),
+                    ],
+                    child: const MenuAcceleratorLabel('&Help'),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
-        body: const Center(
-          child: MainMenu(),
+        Expanded(
+          child: FlutterLogo(
+            size: MediaQuery.of(context).size.shortestSide * 0.5,
+          ),
         ),
-      ),
+      ],
     );
   }
 }
 
-class MainMenu extends StatelessWidget {
-  const MainMenu({super.key});
-
-  void handleMenuSelection(BuildContext context, String value) {
-    if (value == 'Submenu1') {
-      _showSubMenu1(context);
-    } else if (value == 'Submenu2') {
-      _showSubMenu2(context);
-    }
-  }
-
-  static void _showSubMenu1(BuildContext context) {
-    showMenu(
-      context: context,
-      position: const RelativeRect.fromLTRB(200, 200, 200, 200),
-      items: [
-        const PopupMenuItem<String>(
-            value: 'Sub1Opt1', child: Text('Submenu 1 - Opcion 1')),
-        const PopupMenuItem<String>(
-            value: 'Sub1Opt2', child: Text('Submenu 1 - Opcion 2')),
-      ],
-    ).then((value) {
-      if (value != null) {
-        // print('Seleccionaste $value');
-      }
-    });
-  }
-
-  static void _showSubMenu2(BuildContext context) {
-    showMenu(
-        context: context,
-        position: const RelativeRect.fromLTRB(200, 200, 200, 200),
-        items: [
-          const PopupMenuItem<String>(
-              value: 'Sub2Opt1', child: Text('Submenu 2 - Opcion 1')),
-          const PopupMenuItem<String>(
-              value: 'Sub2Opt2', child: Text('Submenu 2 - Opcion 2')),
-        ]).then((value) {
-      if (value != null) {
-        //print('Seleccionaste $value');
-      }
-    });
-  }
+class MenuAcceleratorApp extends StatelessWidget {
+  const MenuAcceleratorApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return PopupMenuButton<String>(
-      onSelected: (value) => handleMenuSelection(context, value),
-      itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
-        const PopupMenuItem<String>(
-            value: 'Submenu1', child: Text('Submenu 1')),
-        const PopupMenuItem<String>(
-            value: 'Submenu2', child: Text('Submenu 2')),
-      ],
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'My MenuApp',
+      theme: ThemeData(useMaterial3: true),
+      home: Shortcuts(
+        shortcuts: <ShortcutActivator, Intent>{
+          const SingleActivator(LogicalKeyboardKey.keyT, control: true):
+              VoidCallbackIntent(() {
+            debugDumpApp();
+          }),
+        },
+        child: const Scaffold(body: SafeArea(child: MyMenuBar())),
+      ),
     );
   }
 }
